1a0393b4d6a19c87101506d5e1a1ee4f1f038999
HomeJa <revanamike620@gmail.com>
2024-07-16 11:03:13 +0300

    👀

--- src/components/starter/header/header.tsx
+++ src/components/starter/header/header.tsx
@@ -1,11 +1,13 @@
 import { component$ } from "@builder.io/qwik";
-import logo from '../../../../public/media/logo.webp';
+import Logo from '/public/media/icons/logo.webp?jsx';
 
 export default component$(() => {
     return (
         <header>
             <nav>
-                <a href="/" class="logo"><span><img src={logo} alt="Real Mission Mouvement Logo" width={79} height={63} /></span></a>
+                <a href="/" class="logo">
+                    <span><Logo/></span>
+                </a>
                 <ul class="nav-links">
                     {/* <li><a href="/pages/galerie">Galerie</a></li>
                     <li><a href="/pages/strategie">Nos strategies</a></li> */}
