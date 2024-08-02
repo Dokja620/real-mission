2a87397e1cc6d9f3f2f1d992bafc698a1afc45b6
HomeJa <revanamike620@gmail.com>
2024-08-02 19:14:49 +0300

    ✨ feat(multilang): works fine👀

--- src/routes/sections/hero/index.tsx
+++ src/routes/sections/hero/index.tsx
@@ -4,7 +4,9 @@ export const Hero = component$(() => {
     return (
         <section id="hero" class="">
             <div class="content container">
-                <h1>La vraie mission <br /> pour le véritable Évangile</h1>
+                <h1 lang-switch="french">La vraie mission <br /> pour le véritable Évangile</h1>
+                <h1 lang-switch="english">The Real mission<br /> for the Real Gospel</h1>
+                
                 <h2>Real Mission Movement</h2>
                 <div class="c-t-a">
                     <a href="">Nous contacter</a>
